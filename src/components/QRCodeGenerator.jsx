2617a5f2db53ab8a974a8f146948cf60d99972b9
Anandh Kumar <accetanandhmca@gmail.com>
2023-09-09 20:12:13 +0530

    Initial Commit

---
+++ src/components/QRCodeGenerator.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/QRCodeGenerator.css'
+
+function QRCodeGenerator() {
+  return (
+    <div>QRCodeGenerator</div>
+  )
+}
+
+export default QRCodeGenerator
